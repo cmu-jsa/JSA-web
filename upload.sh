160e93de3e00197c3310bed82393ef25d9efdf6d
Stan Zhang <stan.zhang2@gmail.com>
2018-03-27 16:46:00 -0400

    Add prompt to upload script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
 
-rsync -avczh --delete dist public cmujsa@cmujsa.com:cmujsa.com/ $@
+SITE=cmujsa.com
+
+USER=cmujsa
+HOST=cmujsa.com
+
+echo "--- UPLOADING TO '$SITE' ---"
+echo -n "Last chance to abort (did you remember to 'npm run dist'?) [y/N] "
+read yn
+case "$yn" in
+    [Yy]*)
+        ;;
+    *)
+        echo "Abort."
+        exit 1
+        ;;
+esac
+unset yn
+
+rsync -avczh --delete dist public $USER@$HOST:$SITE/ $@
 
